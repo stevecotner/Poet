e19a9b8c34fde16f002713d6f78e01a7721b2886
Steve Cotner <stevecotner@gmail.com>
2020-07-12 23:45:41 -0700

    Simplify ActionEvaluating

--- Sources/Poet/ActionEvaluating.swift
+++ Sources/Poet/ActionEvaluating.swift
@@ -10,18 +10,5 @@ import Foundation
 public protocol ActionEvaluating {
     associatedtype Action: EvaluatorAction
     func evaluate(_ action: Action?)
-    func _evaluate(_ action: Action)
-}
-
-extension ActionEvaluating {
-    public func evaluate(_ action: Action?) {
-        if let action = action {
-            breadcrumb(action)
-            _evaluate(action)
-        }
-    }
-    
-    public func breadcrumb(_ action: Action) {
-        debugPrint("Placeholder for breadcrumbing. Evaluator: \(self). Action: \(action.breadcrumbDescription).")
-    }
+    func breadcrumb(_ action: Action)
 }
